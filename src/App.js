9e8399a76a7357bdc78ed11d780029f4ee5b070c
Miguel <miguezaga2@hotmail.com>
2022-11-12 09:17:10 -0500

    add html structure

--- src/App.js
+++ src/App.js
@@ -3,12 +3,34 @@ import { useState } from "react";
 function App() {
   return (
     <div>
-  Build The Community Your Fans Will Love
-
-  Huddle re-imagines the way we build communities. You have a voice, but so does your audience. 
-  Create connections with your users as you engage in genuine discussion. 
-
-  Register
+      <header>
+        <img src="" />
+      </header>
+      <main>
+        <img src="" />
+        <div>
+          <h1>Build The Community Your Fans Will Love </h1>
+          <p>
+            Huddle re-imagines the way we build communities. You have a voice,
+            but so does your audience. Create connections with your users as you
+            engage in genuine discussion.
+          </p>
+          <button>Register</button>
+        </div>
+      </main>
+      <footer>
+        <ul>
+          <li>
+            <a href="#"></a>
+          </li>
+          <li>
+            <a href="#"></a>
+          </li>
+          <li>
+            <a href="#"></a>
+          </li>
+        </ul>
+      </footer>
     </div>
   );
 }

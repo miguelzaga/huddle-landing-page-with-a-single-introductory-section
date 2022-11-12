bbe124525b2cccb8a8b78e0b223f2de7a289b417
Miguel <miguezaga2@hotmail.com>
2022-11-12 08:57:23 -0500

    setup files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import { useState } from "react";
 function App() {
   return (
     <div>
-      <h1>New App</h1>
+  Build The Community Your Fans Will Love
+
+  Huddle re-imagines the way we build communities. You have a voice, but so does your audience. 
+  Create connections with your users as you engage in genuine discussion. 
+
+  Register
     </div>
   );
 }

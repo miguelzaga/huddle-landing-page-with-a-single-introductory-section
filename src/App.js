af8b9ad318c029b69bc87636fce025572d078ade
Miguel <miguezaga2@hotmail.com>
2022-11-12 09:50:31 -0500

    add html classes

--- src/App.js
+++ src/App.js
@@ -3,31 +3,33 @@ import { useState } from "react";
 function App() {
   return (
     <div>
-      <header>
-        <img src="" />
+      <header className="header">
+        <img src="" className="header__logo" />
       </header>
-      <main>
-        <img src="" />
-        <div>
-          <h1>Build The Community Your Fans Will Love </h1>
-          <p>
+      <main className="main">
+        <img src="" className="main__img" />
+        <div className="main__container">
+          <h1 className="main__title">
+            Build The Community Your Fans Will Love{" "}
+          </h1>
+          <p className="main__text">
             Huddle re-imagines the way we build communities. You have a voice,
             but so does your audience. Create connections with your users as you
             engage in genuine discussion.
           </p>
-          <button>Register</button>
+          <button className="main__btn">Register</button>
         </div>
       </main>
-      <footer>
-        <ul>
-          <li>
-            <a href="#"></a>
+      <footer className="footer">
+        <ul className="footer__socials">
+          <li className="footer__socials-li">
+            <a className="footer__socials-a" href="#"></a>
           </li>
-          <li>
-            <a href="#"></a>
+          <li className="footer__socials-li">
+            <a className="footer__socials-a" href="#"></a>
           </li>
-          <li>
-            <a href="#"></a>
+          <li className="footer__socials-li">
+            <a className="footer__socials-a" href="#"></a>
           </li>
         </ul>
       </footer>
